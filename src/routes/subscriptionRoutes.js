e060015be0ad06624583a8715b271a87853efd3a
Kelly-Ann Santerre-Gosselin <kelly.gosselin@mac.home>
2026-08-12 13:21:02 -0400

    Nettoyer route webhook Stripe en double

--- src/routes/subscriptionRoutes.js
+++ src/routes/subscriptionRoutes.js
@@ -7,6 +7,5 @@ router.get('/plans', subscriptionController.getPlans);
 router.get('/current', authenticateToken, subscriptionController.getCurrent);
 router.post('/checkout-session', authenticateToken, subscriptionController.createCheckoutSession);
 router.post('/cancel', authenticateToken, subscriptionController.cancel);
-router.post('/webhook', subscriptionController.webhook);
 
 module.exports = router;
